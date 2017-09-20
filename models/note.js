9edbea8c1de1cbd97dc9965b71569dd0db8d42e5
Jake Thornberry <wjthornberry@gmail.com>
2017-09-20 18:05:07 -0500

    Set up like class activity

---
+++ models/note.js
@@ -0,0 +1,23 @@
+// Require Mongoose
+const mongoose = require('mongoose');
+// Create a schema class
+const Schema = mongoose.Schema;
+
+// Create the Note schema
+let NotesSchema = new Schema({
+    title: {
+        type: String
+    },
+    body: {
+        type: String
+    }
+});
+
+// Mongoose will automatically save the ObjectIds of the notes
+// These ids are referred to in the Article model
+
+// Create the Note model with the NoteSchema
+let Note = mongoose.model('Note', NoteSchema);
+
+// Export the Note model
+module.exports = Note; 
