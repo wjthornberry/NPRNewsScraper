d0bd16b821cee62b984862679113014aa57c6843
Jake Thornberry <wjthornberry@gmail.com>
2017-09-24 21:08:08 -0500

    Add note's info to the page

--- public/app.js
+++ public/app.js
@@ -15,32 +15,36 @@ $('#scrape').on('click', function() {
 });
 
 // On click, push the saved articles to the favorites page
-$()
-
-        // Display the appropriate info on the page
-        $('#articles').append(`<p data-id=' ${data[i]._id} '> ${data[i].title} <br /> ${data[i].linkf} </p>`);
-        }
-    });
+$('#articles').on('click', '#addNote', function() {
+    console.log('Clicked!');
+});
 
 // p tag click
-$(document).on('click', 'p', function() {
+$('thing').on('click', function() {
     // Empty the notes from the note section
     $('#notes').empty();
     // Save the id from the p tag
     let thisId = $(this).attr('data-id');
 
-    // Ajax call for the article
+    // AJAX call for the article
     $.ajax({
         method: 'GET',
         url: '/articles/' + thisId
     })
-    // Once that is done, add the note's information to the page
+    // Once that is done, add the note's info to the page
     .done(function(data) {
         console.log(data);
         // Article title
-        $('#notes').append(`<h2> ${data.title} </h2>'`);
+        $('#notes').append(`<h2> ${data.title} </h2>`);
         // Input to enter a new title
-        $('#notes').append(`<input id='titleinput' name='title' >'`);
+        $('#notes').append('<input id='titleinput' name='title' >');
+        // Text area to add a new note body
+        $('#notes').append('<textarea id='bodyinput' name='body'></textarea>');
+        // A button to submit a new note with the iD of the article saved to it
+        $('#notes').append(`<button data-id=' ${data._id} 'id='savenote'>Save Note</button`);
+    })
+})
+
         // Text area to add a new note body
         $('#notes').append(`<textarea id='bodyinput' name='body'></textarea>`);
         // A button to submit a new note, with the id of the article saved to it
